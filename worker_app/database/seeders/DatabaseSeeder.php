bf36bdfa6b21922742c47a1508eb3d0cb41eca2f
Arnas Twilight <worgenmagic@gmail.com>
2024-11-14 17:15:51 +0300

    30v

--- worker_app/database/seeders/DatabaseSeeder.php
+++ worker_app/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             PositionSeeder::class,
-            WorkerSeeder::class,
+            ProfileSeeder::class,
         ]);
         // User::factory(10)->create();
 
